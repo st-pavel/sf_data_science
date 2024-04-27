9c1a2727db5def0ca968a0039547a534ed1e0495
st-pavel <stepurin.pavel@gmail.com>
2024-04-27 09:20:58 +0300

    Add new Binary search algorithm

---
+++ guess-number-task/game_v3.py
@@ -0,0 +1,32 @@
+def game_core_v3(number: int = 1) -> int:
+    """ Используем алгоритм бинарного поиска для приближения к загаданному числу
+        Binary search algorithm 
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    # Ваш код начинается здесь
+
+    count = 0
+    predict = np.random.randint(1, 101)
+
+    min = 0
+    max = 100
+
+    while number != predict:
+        count += 1
+        if number > predict:
+            min = predict
+            #predict = np.random.randint(min, max)
+            predict = (max+min)//2
+        elif number < predict:
+            max = predict
+            #predict = np.random.randint(min, max)
+            predict = (max+min)//2
+
+
+    # Ваш код заканчивается здесь
+
+    return count
